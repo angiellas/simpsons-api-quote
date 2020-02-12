5dcbb1fee2f2a30d3f3077d359b48784567a2d22
Angeline R <4ng133@gmail.com>
2020-02-12 10:51:34 +0000

    add samplequote

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import axios from "axios";
 import DisplayQuote from "./Components/DisplayQuote";
 
+const sampleQuote = {
+  quote: "By chilling my loins I increase the chances of impregnating my wife.",
+  character: "Apu Nahasapeemapetilon",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+  characterDirection: "Left"
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: {}
+      quote: sampleQuote
     };
     this.getQuote = this.getQuote.bind(this);
   }
